ab0ea07676eb6ae0f799cfd1c0295109893a7c1e
Taylor Keller <takeller17@gmail.com>
2020-04-22 09:14:24 -0600

    Change initialize to handlle alternate input

--- lib/ingredient.rb
+++ lib/ingredient.rb
@@ -1,9 +1,15 @@
 class Ingredient
 
   attr_reader :name, :unit, :calories
-  def initialize(ingredient_info)
-    @name = ingredient_info[:name]
-    @unit = ingredient_info[:unit]
-    @calories = ingredient_info[:calories]
+  def initialize(ingredient_info, unit = nil, calories = nil)
+    if ingredient_info.class == Hash
+      @name = ingredient_info[:name]
+      @unit = ingredient_info[:unit]
+      @calories = ingredient_info[:calories]
+    else
+      @name = ingredient_info
+      @unit = unit
+      @calories = calories
+    end 
   end
 end
