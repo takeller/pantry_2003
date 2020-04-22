bc473673f52ddfb2186acc83d56100eda6931e92
Taylor Keller <takeller17@gmail.com>
2020-04-22 09:20:42 -0600

    Add restock test

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -18,4 +18,20 @@ class PantryTest < MiniTest::Test
   def test_stock_is_empty_by_default
     assert_equal Hash.new, @pantry.stock
   end
+
+  def test_stock_check
+    assert_equal 0, @pantry.stock_check(@ingredient1)
+  end
+
+  def test_restock
+    @pantry.restock(@ingredient1, 5)
+    @pantry.restock(@ingredient1, 10)
+    assert_equal ({@ingredient1 => 15}), @pantry.stock
+    @pantry.restock(@ingredient2, 7)
+    expected = {
+      @ingredient1 => 15,
+      @ingredient2 => 7
+    }
+    assert_equal expected, @pantry.stock 
+  end
 end
