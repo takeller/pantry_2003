877c1de06e81cea76ed7ae8bc6b917d7988c7682
Taylor Keller <takeller17@gmail.com>
2020-04-22 09:04:13 -0600

    Add readable attributes test

---
+++ test/ingredient_test.rb
@@ -0,0 +1,20 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ingredient'
+
+class IngredientTest < MiniTest::Test
+
+  def setup
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+  end
+
+  def test_it_exists
+    assert_instance_of Ingredient, @ingredient1
+  end
+
+  def test_it_has_readable_attributes
+    assert_equal "Cheese", @ingredient1.name
+    assert_equal "oz", @ingredient1.unit
+    assert_equal 50, @ingredient1.calories
+  end
+end
