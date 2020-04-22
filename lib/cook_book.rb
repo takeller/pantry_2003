64178bec3bed30e1aa5cf670ee5a111e9eeeea5b
Taylor Keller <takeller17@gmail.com>
2020-04-22 10:39:18 -0600

    Add date method

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -1,9 +1,10 @@
 require 'pry'
 class CookBook
 
-  attr_reader :recipes
+  attr_reader :recipes, :date
   def initialize
     @recipes = Array.new
+    @date = Date::today.strftime("%m-%d-%Y")
   end
 
   def add_recipe(recipe)
@@ -20,6 +21,6 @@ class CookBook
     calories_by_recipe = @recipes.map do |recipe|
       [recipe, recipe.total_calories]
     end.to_h
-    calories_by_recipe.max_by { |recipe, calories| calories }[0]  
+    calories_by_recipe.max_by { |recipe, calories| calories }[0]
   end
 end
